9933f3af93379fae539a4e6c0c6314ec2d4b557a
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-01-21 16:30:52 -0300

    Atualização do arquivo de exemplo da função MDCard.

--- kivymd_content/9 - Card/1_md_card.py
+++ kivymd_content/9 - Card/1_md_card.py
@@ -1,57 +1,96 @@
-# # Arquivo de exemplo da função MDCard.
-#
-# from kivymd.app import MDApp
-# from kivy.lang import Builder
-# from kivymd_smtplib_tutorial.kivymd_content.config_macros import *
-#
-# KV = '''
-# MDScreen:
-#     md_bg_color: self.theme_cls.backgroundColor
-#
-#     MDBoxLayout:
-#         pos_hint: {"center_x": 0.5, "center_y": 0.5}
-#
-#         MDCard:
-#             style: "elevated"
-#             padding: "4dp"
-#             size_hint: None, None
-#             size: "240dp", "100dp"
-#             theme_shadow_color: "Custom"                    # Valores: 'Primary' e 'Custom'
-#             shadow_color: "green"
-#             theme_bg_color: "Custom"                        # Valores: 'Primary' e 'Custom'
-#             md_bg_color: (30/255, 22/255, 71/255, 1)
-#             md_bg_color_disabled: "grey"
-#             theme_shadow_offset: "Custom"                   # Valores: 'Primary' e 'Custom'
-#             shadow_offset: (1, 0)
-#             theme_shadow_softness: "Custom"                 # Valores: 'Primary' e 'Custom'
-#             shadow_softness: 1
-#             theme_elevation_level: "Primary"                # Valores: 'Primary' e 'Custom'
-#             elevation_level: 5                              # Valores: 1 a 5
-#
-#         MDIconButton:
-#             icon: "dots-vertical"
-#             pos_hint: {"top": 1, "right": 1}
-#
-#         MDLabel:
-#             text: "Elevated"
-#             adaptive_size: True
-#             color: "grey"
-#             pos: "12dp", "12dp"
-#             bold: True
-# '''
-#
-#
-# class MainApp(MDApp):
-#     def build(self):
-#         "Função que cria o App."
-#
-#         self.theme_cls.primary_palette = PALETA_AZUL
-#         self.theme_cls.theme_style = TEMA_CLARO
-#
-#         screen = Builder.load_string(KV)
-#         return screen
-#
-# MainApp().run()
+# Arquivo de exemplo da função MDCard.
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.card import MDCard
+from kivy.properties import StringProperty
+from kivymd_smtplib_tutorial.kivymd_content.config_macros import *
+
+screen_string = '''
+
+# Define o design do widget de cartão.
+<MyCard>
+    padding: "4dp"
+    size_hint: None, None
+    size: "240dp", "100dp"
+
+    MDFloatLayout:
+        
+        # Exibe um botão de ícone no canto superior direito.
+        MDIconButton:
+            icon: "dots-vertical"
+            pos_hint: {"top": 1, "right": 1}
+
+        # Mostra o texto do cartão.
+        MDLabel:
+            
+            # O termo 'root' se refere à classe base na qual o widget foi definido. 
+            # No caso de <MyCard>, o root é a instância da classe MyCard.
+            # O termo 'root.text' refere-se à propriedade text da classe MyCard.
+        
+            text: root.text         
+            adaptive_size: True
+            color: "grey"
+            pos_hint: {"center_x": 0.5, "y": 0}
+            bold: True
+
+MDScreen:
+    theme_bg_color: "Custom"
+    md_bg_color: self.theme_cls.backgroundColor
+
+    MDBoxLayout:
+        id: box
+        adaptive_size: True
+        spacing: "12dp"
+        pos_hint: {"center_x": .5, "center_y": .5}
+'''
+
+# Criação de uma classe que herda de MDCard para criar cartões personalizados.
+class MyCard(MDCard):
+
+    # Adiciona a propriedade text como uma StringProperty.
+    # StringProperty é reativa: qualquer alteração no valor atualiza automaticamente os widgets vinculados.
+    text = StringProperty()
+
+class MyApp(MDApp):
+    def build(self):
+        "Função que cria o App."
+
+        self.theme_cls.primary_palette = PALETA_AZUL
+        self.theme_cls.theme_style = TEMA_CLARO
+
+        screen = Builder.load_string(screen_string)
+        return screen
+
+    def on_start(self):
+        "Função que é chamada automaticamente após o aplicativo iniciar."
+
+        # Adiciona os Cards no id do MDBoxLayout.
+        for style in ("elevated", "filled", "outlined"):
+            self.root.ids.box.add_widget(
+                MyCard(style=style,
+                       text=style.capitalize(),
+                       pos_hint={"center_x": 0.5, "center_y": 0.5},
+                       padding="4dp",
+                       size_hint=(None, None),
+                       size=(200, 300),
+                       theme_bg_color=CUSTOM,           # Valores: 'Primary' e 'Custom'
+                       md_bg_color=BRANCO,              # Cor de preenchimento.
+                       theme_shadow_color=CUSTOM,       # Valores: 'Primary' e 'Custom'
+                       shadow_color=AMARELO,            # Cor de fundo.
+                       theme_shadow_offset="Custom",    # Define o deslocamento da shadow_color em relação ao widget que a projeta.
+                       shadow_offset=(5, 5),            # Valores positivos (x ou y): Movem a sombra para a direita (x) ou para cima (y).
+                                                        # Valores negativos (x ou y): Movem a sombra para a esquerda (x) ou para baixo (y).
+                       md_bg_color_disabled=VERMELHO,   # Define a cor de fundo do cartão quando ele está desativado (disabled=True).
+                       disabled=False,                  # Quando disabled=True, o cartão muda automaticamente para a cor definida em md_bg_color_disabled.
+                       theme_shadow_softness=CUSTOM,    # Valores: 'Primary' e 'Custom'
+                       shadow_softness=1,               # Suavidade da cor de fundo.
+                       theme_elevation_level=CUSTOM,    # Valores: 'Primary' e 'Custom'
+                       elevation_level=5)               # Elevação da cor de fundo.
+            )
+
+
+MyApp().run()
 
 
 
@@ -132,8 +171,8 @@
 # Valores positivos movem a sombra para a direita.
 # Valores negativos movem a sombra para a esquerda.
 # y: deslocamento vertical.
-# Valores positivos movem a sombra para baixo.
-# Valores negativos movem a sombra para cima.
+# Valores positivos movem a sombra para cima.
+# Valores negativos movem a sombra para baixo.
 # Valores possíveis:
 # Qualquer tupla de números inteiros ou de ponto flutuante, como (0, 0), (1, -2), ou (10, 10).
 # Padrão: (0, 2) (caso não especificado).
@@ -159,96 +198,3 @@
 # Descrição: Define o nível de elevação do card, que afeta a aparência da sombra.
 # Valores entre 1 a 5.
 # Observação: Valores maiores aumentam a distância percebida do card em relação ao fundo.
-
-
-from kivy.lang import Builder
-from kivy.properties import StringProperty
-from kivymd_smtplib_tutorial.kivymd_content.config_macros import *
-from kivymd.app import MDApp
-from kivymd.uix.card import MDCard
-
-screen_string = '''
-
-# Define o design do widget de cartão.
-<MyCard>
-    padding: "4dp"
-    size_hint: None, None
-    size: "240dp", "100dp"
-
-    MDRelativeLayout:
-        
-        # Exibe um botão de ícone no canto superior direito.
-        MDIconButton:
-            icon: "dots-vertical"
-            pos_hint: {"top": 1, "right": 1}
-
-        # Mostra o texto do cartão.
-        MDLabel:
-            
-            # O termo 'root' se refere à classe base na qual o widget foi definido. 
-            # No caso de <MyCard>, o root é a instância da classe MyCard.
-            # O termo 'root.text' refere-se à propriedade text da classe MyCard.
-        
-            text: root.text         
-            adaptive_size: True
-            color: "grey"
-            pos: "12dp", "12dp"
-            bold: True
-
-MDScreen:
-    theme_bg_color: "Custom"
-    md_bg_color: self.theme_cls.backgroundColor
-
-    MDBoxLayout:
-        id: box
-        adaptive_size: True
-        spacing: "12dp"
-        pos_hint: {"center_x": .5, "center_y": .5}
-'''
-
-# Criação de uma classe que herda de MDCard para criar cartões personalizados.
-class MyCard(MDCard):
-
-    # Adiciona a propriedade text como uma StringProperty.
-    # StringProperty é reativa: qualquer alteração no valor atualiza automaticamente os widgets vinculados.
-    text = StringProperty()
-
-class MyApp(MDApp):
-    def build(self):
-        "Função que cria o App."
-
-        self.theme_cls.primary_palette = PALETA_AZUL
-        self.theme_cls.theme_style = TEMA_CLARO
-
-        screen = Builder.load_string(screen_string)
-        return screen
-
-    def on_start(self):
-        "Função que é chamada automaticamente após o aplicativo iniciar."
-
-        # Adiciona os Cards no id do MDBoxLayout.
-        for style in ("elevated", "filled", "outlined"):
-            self.root.ids.box.add_widget(
-                MyCard(style=style,
-                       text=style.capitalize(),
-                       pos_hint={"center_x": .5, "center_y":.5},
-                       padding="4dp",
-                       size_hint=(None, None),
-                       size=(200, 300),
-                       theme_bg_color=CUSTOM,
-                       md_bg_color=BRANCO,              # Cor de preenchimento.
-                       theme_shadow_color=CUSTOM,
-                       shadow_color=AMARELO,            # Cor de fundo.
-                       theme_shadow_offset="Custom",    # Define o deslocamento da shadow_color em relação ao widget que a projeta.
-                       shadow_offset=(5, 5),            # Valores positivos (x ou y): Movem a sombra para a direita (x) ou para cima (y).
-                                                        # Valores negativos (x ou y): Movem a sombra para a esquerda (x) ou para cima (y).
-                                                        # Valores negativos (x ou y): Movem a sombra para a esquerda (x) ou para cima (y).
-                       md_bg_color_disabled=VERMELHO,
-                       theme_shadow_softness=CUSTOM,
-                       shadow_softness=1,               # Suavidade da cor de fundo.
-                       theme_elevation_level=CUSTOM,
-                       elevation_level=5)               # Elevação da cor de fundo.
-            )
-
-
-MyApp().run()
