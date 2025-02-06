4cd3bfd3183bc743e9994124f8d45627a0453af8
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-02-06 01:16:09 -0300

    Criação do arquivo de exemplo da função MDNavigationBar.

---
+++ kivymd_content/11 - Navigations/4_navigation_bar.py
@@ -0,0 +1,168 @@
+# Arquivo de exemplo da função MDNavigationBar.
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivy.properties import StringProperty
+from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
+
+screen_string = '''
+<BaseMDNavigationItem>
+    MDNavigationItemIcon:
+        icon: root.icon
+
+    MDNavigationItemLabel:
+        text: root.text
+
+
+<BaseScreen>
+    FitImage:
+        source: f"https://picsum.photos/{root.image_size}/{root.image_size}"
+        size_hint: .9, .9
+        pos_hint: {"center_x": .5, "center_y": .5}
+        radius: dp(24)
+
+
+MDBoxLayout:
+    orientation: "vertical"
+    md_bg_color: self.theme_cls.backgroundColor
+
+    MDScreenManager:
+        id: screen_manager
+
+        BaseScreen:
+            name: "Screen 1"
+            image_size: "1024"
+
+        BaseScreen:
+            name: "Screen 2"
+            image_size: "800"
+
+        BaseScreen:
+            name: "Screen 3"
+            image_size: "600"
+
+
+    MDNavigationBar:
+        on_switch_tabs: app.on_switch_tabs(*args)
+
+        BaseMDNavigationItem
+            icon: "gmail"
+            text: "Screen 1"
+            active: True
+
+        BaseMDNavigationItem
+            icon: "twitter"
+            text: "Screen 2"
+
+        BaseMDNavigationItem
+            icon: "linkedin"
+            text: "Screen 3"
+'''
+
+class BaseMDNavigationItem(MDNavigationItem):
+    icon = StringProperty()
+    text = StringProperty()
+    indicator_color = (1, 1, 0, 1)
+    indicator_duration = 0.5
+
+class BaseScreen(MDScreen):
+    image_size = StringProperty()
+
+class MyApp(MDApp):
+    def build(self):
+        "Função que cria o App."
+
+        self.screen = Builder.load_string(screen_string)
+        return self.screen
+
+    def on_switch_tabs(
+        self,
+        bar: MDNavigationBar,
+        item: MDNavigationItem,
+        item_icon: str,
+        item_text: str,
+    ):
+        self.root.ids.screen_manager.current = item_text
+
+
+MyApp().run()
+
+
+#                               ----- Exemplo de código que não utiliza orientação a objetos -----
+
+# screen_string = '''
+# MDBoxLayout:
+#     id: box
+#     orientation: "vertical"
+#     md_bg_color: self.theme_cls.backgroundColor
+#
+#     MDScreenManager:
+#         id: screen_manager
+#
+#         MDScreen:
+#             name: "Screen 1"
+#
+#             FitImage:
+#                 source: f"https://picsum.photos/{1024}/{1024}"
+#                 size_hint: (0.9, 0.9)
+#                 pos_hint: {"center_x": 0.5, "center_y": 0.5}
+#                 radius: dp(24)
+#
+#
+#         MDScreen:
+#             name: "Screen 2"
+#
+#             FitImage:
+#                 source: f"https://picsum.photos/{800}/{800}"
+#                 size_hint: (0.9, 0.9)
+#                 pos_hint: {"center_x": 0.5, "center_y": 0.5}
+#                 radius: dp(24)
+#
+#
+#         MDScreen:
+#             name: "Screen 3"
+#
+#             FitImage:
+#                 source: f"https://picsum.photos/{600}/{600}"
+#                 size_hint: (0.9, 0.9)
+#                 pos_hint: {"center_x": 0.5, "center_y": 0.5}
+#                 radius: dp(24)
+#
+#     MDNavigationBar:
+#         on_switch_tabs: app.on_switch_tabs(item_text)
+#
+#         MDNavigationItem:
+#             MDNavigationItemIcon:
+#                 icon: "gmail"
+#
+#             MDNavigationItemLabel:
+#                 text: "Screen 1"
+#
+#         MDNavigationItem:
+#             MDNavigationItemIcon:
+#                 icon: "twitter"
+#
+#             MDNavigationItemLabel:
+#                 text: "Screen 2"
+#
+#         MDNavigationItem:
+#             MDNavigationItemIcon:
+#                 icon: "linkedin"
+#
+#             MDNavigationItemLabel:
+#                 text: "Screen 3"
+# '''
+#
+#
+# class MyApp(MDApp):
+#     def build(self):
+#         "Função que cria o App."
+#
+#         self.screen = Builder.load_string(screen_string)
+#         return self.screen
+#
+#     def on_switch_tabs(self, nome_tela):
+#         self.root.ids.screen_manager.current = nome_tela
+#
+# MyApp().run()
