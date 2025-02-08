b1d1c387f90f76041fc391c8e30af7393e18bd5c
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-02-08 17:04:39 -0300

    Atualização do arquivo de exemplo da função MDNavigationRail.

--- kivymd_content/11 - Navigations/5_navigation_rail.py
+++ kivymd_content/11 - Navigations/5_navigation_rail.py
@@ -4,6 +4,62 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.properties import StringProperty
 from kivymd.uix.navigationrail import MDNavigationRailItem
+#
+# screen_string = '''
+# <CommonNavigationRailItem>
+#
+#     MDNavigationRailItemIcon:
+#         icon: root.icon
+#
+#     MDNavigationRailItemLabel:
+#         text: root.text
+#
+# MDScreen:
+#     md_bg_color: self.theme_cls.secondaryContainerColor
+#
+#     MDNavigationRail:
+#         type: "labeled"
+#         anchor: "top"
+#         radius: 20
+#
+#         # Podem ser utilizados os mesmos parâmetros da função MDIconButton.
+#         MDNavigationRailMenuButton:
+#             icon: "menu"
+#
+#         # Podem ser utilizados os mesmos parâmetros da função MDFabButton.
+#         MDNavigationRailFabButton:
+#             icon: "home"
+#
+#         CommonNavigationRailItem:
+#             icon: "folder-outline"
+#             text: "Files"
+#
+#         CommonNavigationRailItem:
+#             icon: "bookmark-outline"
+#             text: "Bookmark"
+#
+#         CommonNavigationRailItem:
+#             icon: "library-outline"
+#             text: "Library"
+# '''
+#
+#
+# class CommonNavigationRailItem(MDNavigationRailItem):
+#     text = StringProperty()
+#     icon = StringProperty()
+#
+#
+# class MyApp(MDApp):
+#     def build(self):
+#         "Função que cria o App."
+#
+#         self.screen = Builder.load_string(screen_string)
+#         return self.screen
+#
+#
+# MyApp().run()
+
+#                               ----- Exemplo de código que não utiliza orientação a objetos -----
 
 screen_string = '''
 <CommonNavigationRailItem>
@@ -14,39 +70,43 @@ screen_string = '''
     MDNavigationRailItemLabel:
         text: root.text
 
-
-MDBoxLayout:
+MDScreen:
+    md_bg_color: self.theme_cls.secondaryContainerColor
 
     MDNavigationRail:
-        type: "selected"
+        type: "labeled"
+        anchor: "top"
+        radius: 20
 
+        # Podem ser utilizados os mesmos parâmetros da função MDIconButton.
         MDNavigationRailMenuButton:
             icon: "menu"
 
-        MDNavigationRailFabButton:
+        # Podem ser utilizados os mesmos parâmetros da função MDFabButton.
+        MDNavigationRailFabButton:          
             icon: "home"
 
-        CommonNavigationRailItem:
-            icon: "folder-outline"
-            text: "Files"
+        MDNavigationRailItem:
+            MDNavigationRailItemIcon:
+                icon: "folder-outline"
+            
+            MDNavigationRailItemLabel:
+                text: "Files"
 
-        CommonNavigationRailItem:
-            icon: "bookmark-outline"
-            text: "Bookmark"
-
-        CommonNavigationRailItem:
-            icon: "library-outline"
-            text: "Library"
-
-    MDScreen:
-        md_bg_color: self.theme_cls.secondaryContainerColor
+        MDNavigationRailItem:
+            MDNavigationRailItemIcon:
+                icon: "bookmark-outline"
+            
+            MDNavigationRailItemLabel:
+                text: "Bookmark"
+                
+        MDNavigationRailItem:
+            MDNavigationRailItemIcon:
+                icon: "library-outline"
+            
+            MDNavigationRailItemLabel:
+                text: "Library"    
 '''
-
-
-class CommonNavigationRailItem(MDNavigationRailItem):
-    text = StringProperty()
-    icon = StringProperty()
-
 
 class MyApp(MDApp):
     def build(self):
