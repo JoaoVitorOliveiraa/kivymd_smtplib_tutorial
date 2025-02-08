eec812f3aa8890fa2f603f81df0d460b64f307e1
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-02-08 01:22:55 -0300

    Atualização do arquivo de exemplo da função MDBadge.

--- kivymd_content/02 - Label/3_badge.py
+++ kivymd_content/02 - Label/3_badge.py
@@ -1,15 +1,15 @@
 # Arquivo de exemplo da função MDBadge.
 
-from kivymd.uix.badge import MDBadge
-from kivymd.uix.label import MDIcon
 from kivymd.app import MDApp
+from kivymd.uix.label import MDIcon
+from kivymd.uix.badge import MDBadge
 from kivymd_smtplib_tutorial.kivymd_content.config_macros import *
 
 class MyApp(MDApp):
     def build(self):
         "Função que cria o App."
 
-        icon_label = MDIcon(MDBadge(text = "12"),
+        icon_label = MDIcon(MDBadge(text = "12", bold = True),
                             icon = "gmail",                                     # Seleciona o ícone.
                             icon_color = ROXO_CLARO,                            # Cor do ícone em (r, g, b, a) ou formato de string.
                             pos_hint = {"center_x": 0.5, "center_y": 0.5})      # Posiciona o ícone na tela, neste caso no meio.
@@ -17,3 +17,10 @@ class MyApp(MDApp):
         return  icon_label  # Retorna o ícone.
 
 MyApp().run()
+
+
+
+#                               ----- DESCRIÇÃO DA FUNÇÃO MDBADGE -----
+
+# MDBadge é um widget da biblioteca KivyMD que exibe uma pequena etiqueta,
+# geralmente usada para mostrar contadores ou notificações sobre ícones ou botões.
